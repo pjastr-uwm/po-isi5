d9643414ec49deec6bd87b683f74ac2bfee463b7
pjastr-uwm <114875756+pjastr-uwm@users.noreply.github.com>
2023-10-16 10:31:15 +0200

    D6

---
+++ lab2/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(suma(4,-5));
+    }
+
+    public static int suma(int a, int b)
+    {
+        return a+b;
+    }
+
+
+}
