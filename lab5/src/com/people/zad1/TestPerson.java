f6435309b8940ed475118475df538d502b1f7fc1
pjastr-uwm <114875756+pjastr-uwm@users.noreply.github.com>
2023-11-06 11:31:45 +0100

    zad1

---
+++ lab5/src/com/people/zad1/TestPerson.java
@@ -0,0 +1,9 @@
+package com.people.zad1;
+
+public class TestPerson {
+    public static void main(String[] args) {
+        System.out.println(Person.getCounter());
+        Person p1 = new Person();
+        System.out.println(Person.getCounter());
+    }
+}
