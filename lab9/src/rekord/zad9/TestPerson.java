ad96497d92f2415b87add1a333e453637f36369d
pjastr-uwm <114875756+pjastr-uwm@users.noreply.github.com>
2023-12-04 10:37:05 +0100

    zad9

---
+++ lab9/src/rekord/zad9/TestPerson.java
@@ -0,0 +1,11 @@
+package rekord.zad9;
+
+public class TestPerson {
+
+    public static void main(String[] args) {
+        Person p1 = new Person("Sylwia", 35);
+        System.out.println(p1);
+        Person p2 = new Person("Krzysztof", -12);
+        System.out.println(p2);
+    }
+}
